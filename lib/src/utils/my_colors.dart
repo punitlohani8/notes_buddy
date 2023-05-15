496823e064e845f6436a22d321d211675c8daa77
Punit Lohani <77602170+punitlohani8@users.noreply.github.com>
2023-05-15 11:15:23 +0530

    colors added

--- lib/src/utils/my_colors.dart
+++ lib/src/utils/my_colors.dart
@@ -5,5 +5,8 @@ class MyColors {
   static const Color white = Colors.white;
   static const Color black = Colors.black;
   static const Color transparent = Colors.transparent;
+  static const Color first = Color(0xffcfff47);
+  static const Color second= Color(0xffd4b4fe);
+  static const Color third = Color(0xffffdba1);
 
 }
