44a562a56ca9f0a11f2b7df0fb36bd40a19df76e
Punit Lohani <77602170+punitlohani8@users.noreply.github.com>
2023-05-15 10:01:36 +0530

    initial commit!!

---
+++ lib/src/utils/my_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color transparent = Colors.transparent;
+
+}
